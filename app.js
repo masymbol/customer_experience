3d754149eea2ca9ec6e1330e0c8a3f9629107f71
Raghu Varma <raghuvarma@masymbol.com>
2014-09-08 19:56:47 +0530

    add google-search and created session id dir

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var bodyParser = require('body-parser');
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -24,10 +26,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({ resave: true,saveUninitialized: true,secret: 'Maasymbol' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var fs = require('fs');
+var directory = "./sessions/";
+
+var app = express();
+
+app.use(session({ resave: true,saveUninitialized: true,secret: 'Maasymbol' }));
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Masymbol' });
+  fs.mkdir(directory+req.session.id, 0777, true, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Directory ' + directory + ' created.');
+  }
+});
 });
 
 module.exports = router;
