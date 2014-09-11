58ac74e84bc6815b5afd03edd5629da639abeb19
Raghu Varma <raghuvarma@masymbol.com>
2014-09-11 13:34:53 +0530

    add search function & integrated java

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
+var exec = require('child_process').exec;
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Masymbol' });
 });
+
+
+router.post('/google-search', function(req, res){
+	var searchQuery = req.body.search;
+	console.log("searchQuery: "+searchQuery);
+	
+	function puts(error, stdout, stderr) { sys.puts(stdout) }
+
+	exec("java -jar /home/raghuvarma/Documents/nodejs_examples/social-media/twitterMultiThread.jar "+searchQuery+" /home/raghuvarma/Documents/nodejs_examples/social-media/user_data", function(err, data){
+			if (err){
+				console.log("Error while running jar file: "+err); 
+
+			}else{
+				console.log("jar jar file running.........");
+			}
+	});
+	
+	/*exec("sudo  Rscript /home/raghuvarma/Desktop/swaps/twitter/rprog3.R", function(err, data){
+			if (err){
+				console.log("Error in Rscript : "+err); 
+
+			}else{
+				console.log("Rscript file running.........");
+			}
+	});
+
+});*/
+
+
 
 module.exports = router;
