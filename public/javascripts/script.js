88b8b0f21f573d080b9b3a4878f9c27d37eecc2d
Raghu Varma <raghuvarma@masymbol.com>
2014-09-22 17:12:52 +0530

    add search form validation, modifying preview file code

---
+++ public/javascripts/script.js
@@ -0,0 +1,16 @@
+jQuery(function(){
+  $("#btn_search_submit").click(function(){
+    $(".error").hide();
+      var hasError = false;
+      var searchReg = /^[a-zA-Z0-9-]+$/;
+      var searchVal = $("#search-text").val();
+      if(searchVal == '') {
+          $("form").after('<span class="error">Please enter a search term.</span>');
+          hasError = true;
+      } else if(!searchReg.test(searchVal)) {
+          $("form").after('<span class="error">Enter valid text.</span>');
+          hasError = true;
+      }
+      if(hasError == true) {return false;}
+    });
+});
