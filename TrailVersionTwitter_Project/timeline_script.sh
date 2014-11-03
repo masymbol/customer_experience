7f7f632c8f9476199319d544ee951fdddad784d7
Raghu Varma <raghuvarma@masymbol.com>
2014-11-03 18:02:15 +0530

    modified code with new jar_files

---
+++ TrailVersionTwitter_Project/timeline_script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+java -jar /home/raghuvarma/Documents/nodejs_examples/social_media/TrailVersionTwitter_Project/timelineOfTrail.jar $1 $2 $3
+
---
+++ TrailVersionTwitter_Project/twitter_script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+java -jar /home/raghuvarma/Documents/nodejs_examples/social_media/TrailVersionTwitter_Project/trailVersionTwitter.jar $1 $2 $3
+
