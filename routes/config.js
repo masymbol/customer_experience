9c2bc5467ae8cd3f1eea3ae6671d8ec5ded6833e
Raghu Varma <raghuvarma@masymbol.com>
2014-11-14 17:06:43 +0530

    modified development mongodb database

--- routes/config.js
+++ routes/config.js
@@ -1,7 +1,7 @@
 module.exports = {
  db: {
    production: "mongodb://trail:eTFJeUhxZ2x1TkVveTkyanZHYTJ2WGQ1ajdsSjc5QTBEakprd1lkcUJjWT0K@172.17.0.38:27017/trail-production",
-   development: "mongodb://localhost/social-media",
+   development: "mongodb://trail:eTFJeUhxZ2x1TkVveTkyanZHYTJ2WGQ1ajdsSjc5QTBEakprd1lkcUJjWT0K@172.17.0.38:27017/trail-production",
    test: "mongodb://localhost/social-media-test",
  }
 };
