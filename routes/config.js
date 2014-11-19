334fec4d721e6edf85ac28ff474660fab0599aa2
Raghu Varma <raghuvarma@masymbol.com>
2014-11-19 12:23:29 +0530

    updated trail_production db

--- routes/config.js
+++ routes/config.js
@@ -1,6 +1,6 @@
 module.exports = {
  db: {
-   production: "mongodb://trail:YmZNNXhVRGoyVE9pNXRHTlZpYVdBVi9xYUxNTHRmOVZjWEFRWjJPd0xEdz0K@172.17.0.208:27017/trail-production",
+   production: "mongodb://trail:eVEzZEtweXBwQzJGNzlGMWpabmwrKzAxeDdPbUgxWEFyODgxSlo3N0pLOD0K@172.17.0.208:27017/trail-production",
    development: "mongodb://localhost/social-media",
    test: "mongodb://localhost/social-media-test",
  }
