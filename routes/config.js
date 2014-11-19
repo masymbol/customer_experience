0c6b0d6c966e2eec84581fada6b94d43cff235ba
Raghu Varma <raghuvarma@masymbol.com>
2014-11-19 12:36:03 +0530

    updated trail_production key

--- routes/config.js
+++ routes/config.js
@@ -1,6 +1,6 @@
 module.exports = {
  db: {
-   production: "mongodb://trail:eVEzZEtweXBwQzJGNzlGMWpabmwrKzAxeDdPbUgxWEFyODgxSlo3N0pLOD0K@172.17.0.208:27017/trail-production",
+   production: "mongodb://trail:@172.17.1.50:27017/trail-production-production",
    development: "mongodb://localhost/social-media",
    test: "mongodb://localhost/social-media-test",
  }
